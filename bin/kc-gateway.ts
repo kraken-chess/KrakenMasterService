523f4f59be8b352bc16693fb075d4d9fe3fc9c73
Nikhil Kumar <nkumar0727@gmail.com>
2020-05-25 20:50:15 -0700

    Setup basic Cognito user pool and client

--- bin/kc-gateway.ts
+++ bin/kc-gateway.ts
@@ -1,7 +1,33 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+import * as dotenv from 'dotenv';
 import { KcGatewayStack } from '../lib/kc-gateway-stack';
 
+// configure .env file environment variable
+dotenv.config();
+
+const environment: cdk.Environment = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+const isEnvironmentProvided: boolean =
+  environment.account != undefined && environment.region != undefined;
+
+console.log(
+  "CDK Environment configurations: Account=%s, Region=%s",
+  environment.account,
+  environment.region
+);
+
+if (!isEnvironmentProvided) {
+  throw Error(
+    "CDK Environment could not be properly configured. \
+    Please ensure Environment Variables are configured correctly: \
+    <CDK_DEFAULT_ACCOUNT, CDK_DEFAULT_REGION>"
+  );
+}
+
 const app = new cdk.App();
-new KcGatewayStack(app, 'KrakenChessGatewayStack');
+new KcGatewayStack(app, 'KrakenChessGatewayStack', {
+    env: environment
+});
--- lib/kc-gateway-stack.ts
+++ lib/kc-gateway-stack.ts
@@ -1,9 +1,46 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { Mfa, UserPool, IUserPoolClient } from "@aws-cdk/aws-cognito";
+import { Duration } from "@aws-cdk/core";
 
 export class KcGatewayStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const krakenChessUserPool: UserPool = new UserPool(
+      this,
+      "KrakenChessUserPool",
+      {
+        userPoolName: "kraken-chess-user-pool",
+        mfa: Mfa.OFF,
+        passwordPolicy: {
+          tempPasswordValidity: Duration.days(1),
+        },
+        requiredAttributes: {
+          email: true,
+        },
+        selfSignUpEnabled: true,
+        signInAliases: {
+          username: true,
+          email: true
+        },
+      }
+    );
+
+    const krakenChessClient: IUserPoolClient = krakenChessUserPool.addClient(
+      "KrakenChessClient",
+      {
+        userPoolClientName: "kraken-chess-client",
+      }
+    );
+
+    new cdk.CfnOutput(this, "KrakenChessUserPoolClientId", {
+      value: krakenChessClient.userPoolClientId,
+      exportName: "KrakenChessUserPoolClientId",
+    });
+
+    new cdk.CfnOutput(this, "KrakenChessUserPoolId", {
+      value: krakenChessUserPool.userPoolId,
+      exportName: "KrakenChessUserPoolId",
+    });
   }
 }
